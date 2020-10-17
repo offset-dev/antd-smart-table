25a70639bc693816dc076ea17fb5c0541343de68
Luis Rodriguez <luisrodriguez.7@icloud.com>
2020-10-17 13:28:11 -0500

    Updates

---
+++ precache-manifest.7095a501dd2b4d98533d0766191e6ac3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb16bd3f6f6dceef640f970f683c7f19",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4ac0c4665ed9e4631d86",
+    "url": "./static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "6647067a7f573285c967",
+    "url": "./static/js/2.0e4fe1c1.chunk.js"
+  },
+  {
+    "revision": "721f8831cff91610b4df7392aff4d626",
+    "url": "./static/js/2.0e4fe1c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ac0c4665ed9e4631d86",
+    "url": "./static/js/main.c607a2c0.chunk.js"
+  },
+  {
+    "revision": "6f238ab296c1c42b5082",
+    "url": "./static/js/runtime-main.4658fe59.js"
+  }
+]);
